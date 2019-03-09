2ebd09320727a02ae89ae2f018e8ceeaf492bbc4
mariajosebare <mara2013.mjb@gmail.com>
2019-03-09 04:28:12 -0300

    base de datos con los datos ingresados mdm3

---
+++ app/data/usuariosmdm3.sql
@@ -0,0 +1,7 @@
+USE mdm3;
+INSERT INTO usuario (ci,nombre,apellido,fecha_nacimiento,email,telefono,calle,numero_puerta,esquina_1,esquina_2) VALUES
+(42481555,"María Carolina","Gherzo Ferreira",21/03/1989,"carolinagherzo@gmail.com",091400188,"Emilio Raña",2968,"Pedro Vidal","José Catala"),
+(46488171,"Cecilia","Varela",12/03/1994,"cjvs-94@hotmail.com",094025206,"Manuel Meléndez",3980,"Santiago Sierra","Rafael Hortiguera"),
+(50884965,"Ana Belen","Casalas",21/10/1994,"belcasalas@gmail.com",099063738," 4 de Julio",3401,"Luis Alberto de Herrera","Julio Cesar"),
+(42826561,"María José","Bareño",10/12/1976,"mara2013.mjb@gmail.com",098214589,"Dr José María Penco",3295,"Dr José Pugnalini","Dr Fermin Ferreira"),
+(25963758,"Pablo Martín","Tort",01/08/1978,"pablotort@hotmail.com",098774722,"Avenida Millán","5269a","Avenida Luis Alberto de Herrera","Reyes");
